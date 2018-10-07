ed0a442040ed15a2348876179316071886676b1a
谢代彬 <xiedaibin@sina.cn>
2018-10-07 23:57:19 +0800

    基础服务

---
+++ BasicsService/Controllers/HealthController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicsService.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    public class HealthController : Controller
+    {
+        /// <summary>
+        /// 主健康检查
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("/health")]
+        public IActionResult KeyHealth()
+        {
+            return Content("BasicsService KeyHealth检查成功！");
+        }
+    }
+}
