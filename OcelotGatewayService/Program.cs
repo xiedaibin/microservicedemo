f1fb02455f03d6f841122695268f0ffbeae0a006
谢代彬 <xiedaibin@sina.cn>
2018-10-08 18:33:14 +0800

    熔断

---
+++ OcelotGatewayService/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace OcelotGatewayService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            IWebHostBuilder builder = WebHost.CreateDefaultBuilder(args);
+            builder.ConfigureServices(s =>
+            {
+                s.AddSingleton(builder);
+            });
+            builder
+                 .ConfigureLogging((hostingContext, logging) =>
+                 {
+                     //add your logging
+                 })
+                 .UseKestrel()
+                 .UseUrls("http://*:6800")
+                 .ConfigureAppConfiguration(configurationbuilder =>
+                 {
+                     configurationbuilder.AddJsonFile("appsettings.json");
+                     configurationbuilder.AddJsonFile("configuration.json");
+                 })
+                 .UseStartup<Startup>();
+            return builder;
+        }
+
+    }
+}
---
+++ OcelotGatewayService/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+
+namespace OcelotGatewayService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+
+            //设置Ocelot相关配置  OcelotPolly 依赖Polly
+            services
+                .AddOcelot(Configuration)
+                .AddPolly();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            await app.UseOcelot();
+        }
+    }
+}
